90cfee55a39336ebe0f508614af3c608b6736b7f
Raphaël Tétreault <raphaeltetreault@outlook.com>
2022-04-30 22:57:02 -0400

    Add very basic Tiled TMX file parsing

---
+++ src/tiled-to-bin/tiled-to-bin/Options.cs
@@ -0,0 +1,21 @@
+﻿using CommandLine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tiled_to_bin
+{
+    public class Options
+    {
+        private static class Help
+        {
+            public const string Input =
+                "The input Tiled XML file";
+        }
+
+        [Option('i', "input", Required = true, HelpText = Help.Input)]
+        public string Input { get; set; } = string.Empty;
+    }
+}
---
+++ src/tiled-to-bin/tiled-to-bin/Program.cs
@@ -0,0 +1,46 @@
+﻿using CommandLine;
+using System;
+using System.IO;
+using System.Linq;
+using System.Xml;
+
+namespace tiled_to_bin
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Parser.Default.ParseArguments<Options>(args)
+                .WithParsed(RunOptions);
+        }
+
+        public static void RunOptions(Options options)
+        {
+            string inputPath = Path.GetFullPath(options.Input);
+            var isInvalidFile = !File.Exists(inputPath);
+            if (isInvalidFile)
+            {
+                Console.WriteLine("Input file is invalid!", inputPath);
+                Console.WriteLine(inputPath);
+                return;
+            }    
+
+            string xml = File.ReadAllText(inputPath);
+            var document = new XmlDocument();
+            document.LoadXml(xml);
+
+            var map = document.GetElementsByTagName("map")[0];
+            Console.WriteLine(map.Name);
+            Console.WriteLine(map.InnerText);
+
+            var layers = document.GetElementsByTagName("layer");
+            foreach (XmlNode layer in layers)
+            {
+                var name = layer.Attributes.GetNamedItem("name").InnerText;
+                Console.WriteLine(name);
+                Console.WriteLine(layer.InnerText);
+            }
+        }
+
+    }
+}
